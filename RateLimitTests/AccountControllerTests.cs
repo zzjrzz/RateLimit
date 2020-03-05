05068150d85d445dc5347411ba9feb52ab626387
Debbie <nypdpa@gmail.com>
2020-03-05 21:00:33 +1100

    Add controller integration test

---
+++ RateLimitTests/AccountControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RateLimit;
+using Xunit;
+
+namespace RateLimitTests
+{
+    public class AccountControllerTests: IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AccountControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/account")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
